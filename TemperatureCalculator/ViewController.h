c46be495cb4184e867d6ce64d6e423a439781fce
Maryna Isaienko <macprobp@gmail.com>
2023-05-23 13:00:39 -0700

    update

--- TemperatureCalculator/ViewController.h
+++ TemperatureCalculator/ViewController.h
@@ -9,6 +9,21 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segControl;
+
+@property (weak, nonatomic) IBOutlet UILabel *enterLabel;
+
+
+- (IBAction)convert:(id)sender;
+
+- (IBAction)switchConversion:(id)sender;
+
 
 @end
 
